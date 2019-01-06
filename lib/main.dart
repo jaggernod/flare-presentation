151be682cb47b2c9436c09b619b799cca1eb866b
Pawel Polanski <jaggernod@gmail.com>
2019-01-06 17:46:24 +0100

    Added Buka

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-import "package:flare_flutter/flare_actor.dart";
+import 'package:flare_flutter/flare.dart';
+import 'package:flare_flutter/flare/math/mat2d.dart';
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
@@ -15,54 +17,183 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Flare Presentation',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page New New'),
+      home: GrokePage(title: 'Groke Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class GrokePage extends StatefulWidget {
+  GrokePage({
+    Key key,
+    this.title,
+  }) : super(key: key);
+
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _GrokePageState createState() => _GrokePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  String _animationName = "idle";
-
+class _GrokePageState extends State<GrokePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.grey,
-        appBar: AppBar(title: Text(widget.title)),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Expanded(
-                  child: FlareActor(
-                "assets/Filip.flr",
-                alignment: Alignment.center,
-                fit: BoxFit.contain,
-                animation: _animationName,
-              ))
-            ],
-          ),
+        body: Column(
+          children: <Widget>[
+            Expanded(child: const Groke()),
+          ],
         ));
+  }
+}
+
+class Groke extends StatefulWidget {
+  const Groke({Key key}) : super(key: key);
+
+  @override
+  _GrokeState createState() => _GrokeState();
+}
+
+enum Mood { scare, smile }
+
+const _kRubbingThreshold = 10000;
+
+class _GrokeState extends State<Groke> implements FlareController {
+  double _speed = 2.0;
+  double _scareFactorAmount = 0.1;
+  double _scareTime = 0.0;
+  double _smileTime = 0.0;
+
+  double _smileFactorAmount = 0.0;
+
+  ActorAnimation _scareAnimation;
+  ActorAnimation _smileAnimation;
+
+  Mood _mood;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _mood = Mood.scare;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RubDetector(
+      isEnabled: _mood == Mood.scare,
+      onRubbed: () {
+        setState(() => _mood = Mood.smile);
+      },
+      child: FlareActor(
+        'assets/groke.flr',
+        alignment: Alignment.center,
+        fit: BoxFit.contain,
+        animation: 'HeadTilt',
+        controller: this,
+      ),
+    );
+  }
+
+  @override
+  bool advance(FlutterActorArtboard artboard, double elapsed) {
+    _scareTime += elapsed * _speed;
+    _smileTime += elapsed * _speed;
+
+    if (_mood == Mood.smile) {
+      if (_scareFactorAmount != 0) {
+        // We do not want to disturb the Scary animation so we wait until it ends
+        if (_scareTime % _scareAnimation.duration < 0.1) {
+          _scareFactorAmount = 0;
+          _smileFactorAmount = 1.0;
+          _scareTime = 0;
+        }
+      }
+    }
+
+    _scareAnimation.apply(
+      _scareTime % _scareAnimation.duration,
+      artboard,
+      _scareFactorAmount,
+    );
+    _smileAnimation.apply(
+      _smileTime % _smileAnimation.duration,
+      artboard,
+      _smileFactorAmount,
+    );
+
+    return true;
+  }
+
+  @override
+  void initialize(FlutterActorArtboard artboard) {
+    // The scary animation
+    _scareAnimation = artboard.getAnimation('Scare');
+
+    // The smiling animation
+    _smileAnimation = artboard.getAnimation('Smile');
+  }
+
+  @override
+  void setViewTransform(Mat2D viewTransform) {}
+}
+
+class RubDetector extends StatefulWidget {
+  final Widget child;
+  final bool isEnabled;
+  final VoidCallback onRubbed;
+
+  const RubDetector({
+    Key key,
+    @required this.child,
+    @required this.onRubbed,
+    this.isEnabled = false,
+  })  : assert(child != null),
+        assert(onRubbed != null),
+        super(key: key);
+
+  @override
+  RubDetectorState createState() => RubDetectorState();
+}
+
+class RubDetectorState extends State<RubDetector> {
+  double _initial;
+
+  double _distance;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _initial = 0.0;
+    _distance = 0.0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onPanStart: (DragStartDetails details) {
+        _initial = details.globalPosition.dx;
+        _distance = 0;
+      },
+      onPanUpdate: (DragUpdateDetails details) {
+        double distance = details.globalPosition.dx - _initial;
+        _distance += distance.abs();
+
+        if (widget.isEnabled && _distance > _kRubbingThreshold) {
+          widget.onRubbed();
+        }
+      },
+      onPanEnd: (DragEndDetails details) {
+        _initial = 0.0;
+        _distance = 0;
+      },
+      child: widget.child,
+    );
   }
 }
