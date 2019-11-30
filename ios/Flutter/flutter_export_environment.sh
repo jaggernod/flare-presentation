3bd2e1de428db4f7c1a966500efa5d9be76ca04b
Pawel Polanski <jaggernod@gmail.com>
2019-11-30 10:48:36 +0100

    Curves

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/jaggernod/work/tools/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/jaggernod/work/projects/flare-presentation"
+export "FLUTTER_APPLICATION_PATH=/home/jaggernod/work/projects/flare_presentation"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
